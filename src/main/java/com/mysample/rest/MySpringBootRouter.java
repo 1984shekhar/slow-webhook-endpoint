148bf2ab4e3f012279fa2e7769ae68a5f41ce66f
chandrashekhar <chandrashekharpandey@hotmail.com>
2023-01-24 22:47:48 +0530

    slow webhook endpoint

---
+++ src/main/java/com/mysample/rest/MySpringBootRouter.java
@@ -0,0 +1,30 @@
+package com.mysample.rest;
+
+import org.apache.camel.PropertyInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+	@PropertyInject(value = "timeOut", defaultValue = "5000")
+	private int timeOut;
+
+    @Override
+    public void configure() {
+		/*
+		 * from("timer:hello?period={{timer.period}}").routeId("hello")
+		 * .transform().method("myBean", "saySomething")
+		 * .filter(simple("${body} contains 'foo'")) .to("log:foo") .end()
+		 * .to("stream:out");
+		 */
+    	System.out.println("Timeout configured is " + timeOut);
+        from("undertow:http://{{serviceuri}}/myapp/myservice").log("Incoming Request: Headers: ${in.headers}" ).delay(timeOut).log("Outgoing Request: Headers: ${in.headers}" + "Body: ${in.body}");
+        //from("")
+    }
+
+}
